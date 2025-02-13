534ace63460cbb6a196f524d57e0d8c4fc2cb333
Jesse Vaerendonck <jesse.vaerendonck@student.hogent.be>
2025-02-13 18:17:16 +0100

    favicon, color change notif, metadata change

--- sdp2/app/Landing/Components/sidebar.tsx
+++ sdp2/app/Landing/Components/sidebar.tsx
@@ -36,7 +36,7 @@ export function Sidebar() {
       <div className="flex items-center gap-2 text-gray-400 hover:text-white transition-colors cursor-pointer">
         <Bell size={20} />
         <span>Notifications</span>
-        <span className="flex h-5 w-5 items-center justify-center rounded-full bg-redAccent text-xs text-white">3</span> {/* Dit is een vb, later checken of er nieuwe notifs zijn --> dan tonen */}
+        <span className="flex h-5 w-5 items-center justify-center rounded-full bg-white text-xs text-redAccent">3</span> {/* Dit is een vb, later checken of er nieuwe notifs zijn --> dan tonen */}
       </div>
     </aside>
   )
--- sdp2/app/layout.tsx
+++ sdp2/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 
 
 export const metadata: Metadata = {
-  title: "Shopfloor Delaware",
+  title: "Shopfloor",
   description: "A shopfloor application for Delware",
 };
 
