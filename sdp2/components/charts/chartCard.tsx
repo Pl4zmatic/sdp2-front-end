c5ab5c2614a52da10a7e28c12a4a0359ffc4d6f7
AnthonyDavidHogent <anthony.david@student.hogent.be>
2025-05-06 23:29:16 +0200

    tabs toegevoegd

--- sdp2/components/charts/chartCard.tsx
+++ sdp2/components/charts/chartCard.tsx
@@ -15,7 +15,7 @@ interface props {
 
 export default function ChartCard({ children, title }: props) {
   return (
-    <Card className="flex flex-col sm:w-full md:w-[45%] lg:w-[25%] aspect-video bg-[var(--navy)] text-white border-[var(--navy)] shadow-xl rounded-lg">
+    <Card className="flex flex-col sm:w-full md:w-[45%] lg:w-[32%] aspect-video bg-[var(--navy)] text-white border-[var(--navy)] shadow-xl rounded-lg">
       <CardHeader className="bg-[var(--lightestNavy)] rounded-t-lg p-4">
         {title}
       </CardHeader>
