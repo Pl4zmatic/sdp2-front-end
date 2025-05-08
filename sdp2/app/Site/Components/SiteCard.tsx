dffffe6d549f6259fc9c1a676991d1b7048cfc4f
WoutS <wout.schelstraete@student.hogent.be>
2025-05-08 16:28:05 +0200

    fix

--- sdp2/app/Site/Components/SiteCard.tsx
+++ sdp2/app/Site/Components/SiteCard.tsx
@@ -9,31 +9,39 @@ import {
 import { Button } from "@/components/ui/button";
 import { MapPin, Factory, User } from "lucide-react";
 import Link from "next/link";
+import { Plant } from "@/app/types/Plant";
+import {getById} from "../../../api/index"
+import useSWR from "swr";
 
 interface SiteCardProps {
-  siteId: number;
-  siteNaam?: string;
-  siteAdres?: string;
+  site: Plant
   aantalMachines?: number;
   verantwoordelijke?: string;
 }
 
+
+
 export default function SiteCard({
-  siteId,
-  siteNaam = "Antwerpen",
-  siteAdres = "Antwerpen 23, 9140 Elversele",
+  site,
   aantalMachines = 0,
   verantwoordelijke = "",
 }: SiteCardProps) {
+
+  const {
+    data: machines = [],
+    isLoading: isLoadingMachines,
+    error: errorMachines,
+  } = useSWR(`sites/${site?.NAME}/machines`, getById)
+  
   return (
     <Card className="bg-delawareRed dark:bg-navy border-0 text-white dark:text-white hover:shadow-lg transition-all duration-300 w-full flex-1">
       <CardHeader className="pb-2">
         <CardTitle className="text-xl font-bold text-white">
-          {siteNaam}
+          {site.NAME}
         </CardTitle>
         <CardDescription className="text-white/80 flex items-start">
           <MapPin size={16} className="mr-1 text-white/80 shrink-0 mt-1" />
-          <span className="break-words">{siteAdres}</span>
+          <span className="break-words">{site.ADDRESS}</span>
         </CardDescription>
       </CardHeader>
       <CardContent className="pb-4">
@@ -45,7 +53,7 @@ export default function SiteCard({
                 Machines:
               </span>
             </div>
-            <span className="md:ml-2 break-words">{aantalMachines}</span>
+            <span className="md:ml-2 break-words">{machines.length}</span>
           </div>
           <div className="text-white/80 flex flex-col md:flex-row md:items-start">
             <div className="flex items-center">
@@ -59,7 +67,7 @@ export default function SiteCard({
         </div>
       </CardContent>
       <CardFooter className="pt-0">
-        <Link href={`${siteId}`}>
+        <Link href={`${site.ID}`}>
           <Button className="bg-white hover:bg-white/90 dark:bg-lightestNavy dark:hover:bg-blueTransparant text-delawareRed dark:text-white border-0 transition-colors w-full">
             Select Plant
           </Button>
