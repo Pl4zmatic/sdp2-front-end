70bdb6e7a793f183f343964a066dc67a3518f405
AnthonyDavidHogent <anthony.david@student.hogent.be>
2025-05-14 23:51:42 +0200

    Css fix
    header kleur van kpi card bleef grijs in dark als light, slecht contrast

--- sdp2/components/charts/chartCard.tsx
+++ sdp2/components/charts/chartCard.tsx
@@ -16,7 +16,7 @@ interface props {
 export default function ChartCard({ children, title }: props) {
   return (
     <Card className="flex flex-col sm:w-full md:w-[45%] lg:w-[32%] aspect-video dark:bg-[var(--navy)] dark:text-white bg-white dark:border-[var(--navy)] shadow-xl rounded-lg">
-      <CardHeader className="text-white bg-bgGray text-darkGray font-semibold dark:bg-[var(--lightestNavy)] rounded-t-lg p-4">
+      <CardHeader className="text-white bg-bgGray text-darkGray dark:text-white font-semibold dark:bg-[var(--lightestNavy)] rounded-t-lg p-4">
         {title}
       </CardHeader>
       <CardContent className="flex-1 py-4 content-center text-black dark:text-white">
