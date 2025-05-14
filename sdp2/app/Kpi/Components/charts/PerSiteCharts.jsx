49d5b53edd8efd09df216dd45e9fa2b40ddee6a0
AnthonyDavidHogent <anthony.david@student.hogent.be>
2025-05-14 17:40:52 +0200

    Grafieken per sites + data via api calls

---
+++ sdp2/app/Kpi/Components/charts/PerSiteCharts.jsx
@@ -0,0 +1,102 @@
+import useSWR from "swr";
+import { getAll } from "@/api/index.js";
+
+import BarChart from "@/components/charts/barchart";
+import FilteredAreaChart from "@/components/charts/filteredAreaChart";
+import PieChart from "@/components/charts/pieChart";
+import RadarChart from "@/components/charts/radarChart";
+import RadialChart from "@/components/charts/radialChart";
+import LineChart from "@/components/charts/lineChart";
+import RadialChartMultiple from "@/components/charts/radialChartMultiple";
+import BarChartStacked from "@/components/charts/barChartStacked";
+
+export function TotalProducedByProduct({ siteName }) {
+  const { data, isLoading, error } = useSWR(`kpi/${siteName}/produced`, getAll);
+
+  if (error) return <div className="text-white">Error</div>;
+
+  return isLoading ? (
+    <div className="text-white">Loading...</div>
+  ) : (
+    <PieChart
+      title="Total Produced By Product"
+      chartData={data}
+      nameKey={"productName"}
+      dataKey="produced"
+    ></PieChart>
+  );
+}
+
+export function AverageThroughputByProduct({ siteName }) {
+  const { data, isLoading, error } = useSWR(
+    `kpi/${siteName}/throughput`,
+    getAll
+  );
+
+  if (error) return <div className="text-white">Error</div>;
+
+  return isLoading ? (
+    <div className="text-white">Loading...</div>
+  ) : (
+    <BarChart
+      title="Average Throughput Rate Per Product By Site"
+      chartData={data}
+      axisName={"productName"}
+      horizontal={true}
+    />
+  );
+}
+
+export function AverageAttainmentByProduct({ siteName }) {
+  const { data, isLoading, error } = useSWR(
+    `kpi/${siteName}/attainment`,
+    getAll
+  );
+
+  if (error) return <div className="text-white">Error</div>;
+
+  return isLoading ? (
+    <div className="text-white">Loading...</div>
+  ) : (
+    <BarChartStacked
+      title="Average Attainment Per Product in %"
+      chartData={data}
+      axisName={"productName"}
+      valueKeys={["target", "produced"]}
+      labelKey="attainment"
+    />
+  );
+}
+
+export function DefectsByProduct({ siteName }) {
+  const { data, isLoading, error } = useSWR(`kpi/${siteName}/defects`, getAll);
+
+  if (error) return <div className="text-white">Error</div>;
+
+  return isLoading ? (
+    <div className="text-white">Loading...</div>
+  ) : (
+    <RadialChartMultiple
+      title="Defects Per Product"
+      chartData={data}
+      nameKey={"productName"}
+      dataKey="maintenances"
+    />
+  );
+}
+
+export function ProductionCostByProduct({ siteName }) {
+  const { data, isLoading, error } = useSWR(`kpi/${siteName}/productionCost`, getAll);
+
+  if (error) return <div className="text-white">Error</div>;
+
+  return isLoading ? (
+    <div className="text-white">Loading...</div>
+  ) : (
+    <RadarChart
+      title="Production Cost Per Product"
+      chartData={data}
+      xAxisName="productName"
+    />
+  );
+}
